087d856efb1afdae21692d17acafabbb60848290
LukasPrograma <lplucaspalacios@gmail.com>
2022-07-08 11:46:55 -0300

    algo

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,25 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	/*Lucas Leonardo Palacios Bulige
+	Div X
+	no entregado
+	Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", NO HACER NADA,
+	pero si no es asi, y es soltero y no es menor, mostrar el siguiente mensaje: 'Es soltero y no es menor.'*/
 
+	//Declaro variable
+	let edad;
+	let estadoCivil;
+
+	//Ingreso variable
+	edad = document.getElementById("txtIdEdad").value;
+	estadoCivil = document.getElementById("estadoCivil").value;
+	//Parseo el dato
+	edad = parseInt(edad);
+
+	//Procesamiento de datos
+	if(edad<18 && estadoCivil != "Soltero"){
+		alert("No hacer nada.");
+	}
+	
 
 }//FIN DE LA FUNCIÓN
