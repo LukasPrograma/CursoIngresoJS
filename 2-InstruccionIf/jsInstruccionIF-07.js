d0f2322b99d8a680938785f00abbfa5321c4eab2
LukasPrograma <lplucaspalacios@gmail.com>
2022-07-08 09:02:30 -0300

    algo

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,23 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	/*Lucas Leonardo Palacios Bulige
+	Div X
+	no entregado
+	Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", mostrar el siguiente mensaje: 'Es muy pequeño para NO ser soltero.'.*/
 
+	//Declaro variable
+	let edad;
+	let estadoCivil;
+
+	//Ingreso variable
+	edad = document.getElementById("txtIdEdad").value;
+	estadoCivil = document.getElementById("estadoCivil").value;
+	//Parseo el dato
+	edad = parseInt(edad);
+
+	//Procesamiento de datos
+	if(edad<18 && estadoCivil != "Soltero"){
+		alert("Es muy pequeño para NO ser soltero.");
+	}
 
 }//FIN DE LA FUNCIÓN
