d20309d437f356c85de607f1c8833dca6a387bdc
LukasPrograma <lplucaspalacios@gmail.com>
2022-07-12 15:27:12 -0300

    Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,25 @@
 function mostrar()
 {
+/*al seleccionar un mes informar.
+si es Enero: "que comiences bien el año!!!."
+si es Marzo: "a clases!!!."
+si es Julio: "se vienen las vacaciones!!!."
+si es Diciembre: "Felices fiesta!!!."*/
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-
-
-
+	let mesDelAño =txtIdMes.value;
+	if(mesDelAño == "Enero"){
+		alert("que comiences bien el año!!!.")
+	}
+	else if(mesDelAño == "Marzo"){
+		alert("a clases!!!.")
+	}
+	else if(mesDelAño == "Julio"){
+		alert("se vienen las vacaciones!!!.")
+	}
+	else if(mesDelAño == "Diciembre"){
+		alert("Felices fiesta!!!.")
+	}
+	else{
+		alert(txtIdMes.value);
+	}
 }//FIN DE LA FUNCIÓN
